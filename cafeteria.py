621248ea68531924f511088a0f8117e67672a664
JeroM <jeronimomurillomontes@gmail.com>
2026-09-17 10:23:41 -0500

    Añado codigo de cafeteria

---
+++ cafeteria.py
@@ -0,0 +1,11 @@
+print("=========================")
+print("SISTEMA PEDIDOS CAFETERÍA")
+print("=========================")
+
+producto = input("Ingrese el nombre del producto: ")
+precio = float(input("Ingrese el precio del producto: "))
+
+print()
+print("Pedido registrado")
+print("producto", producto)
+print("Precio: $", precio)
